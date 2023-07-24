c94d34411fa1d1d07b3e713739d5dc359449d3c0
umutesimagnificat@gmail.com <umutesimagnificat@gmail.com>
2023-07-24 12:04:27 +0200

    one and three

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,59 @@
 console.log("Exercise 1 - Word Highlighter");
+let para = document.getElementById("myParagraph")
+let arr = para.textContent.split(" ");
+let arr2=[];
+let fiveWords=[];
+// let mostOccuring=[];
+window.addEventListener("DOMContentLoaded", function(){
+    var count=arr.reduce((acc, curr)=>{
+        if(!acc[curr]){
+            acc[curr]=0
+        }
+        acc[curr]++;
+        return acc
+    },{}) 
+        
+    // console.log(count)
+    let arr2 = Object.entries(count).sort((a,b)=> b[1]-a[1])
+    for(let j=1; j<6; j++){
+        fiveWords.push(arr2[j][0])
+    }
+    console.log(fiveWords)
+    let word = para.childNodes
+    word.forEach((elt)=>{
+        if(elt.nodeName === "#text"){
+            let segment = elt
+            console.log (segment)
+            // highlight(elt)
+        }
+    })
+
+    // function highlight(text){
+    //     for
+    // }
+    
+    // console.log(word)
+    // Each entry!!
+    // for (let i in arr2){
+    //     console.log (arr2[i][0])        
+    // }
+
+    // for (let i in count){
+    //     arr2.push(count[i])
+
+    // }
+    // console.log(arr2.sort((a,b) => b-a))
+    // for (let j=1; j<6;j++){
+    //     for(let prop of count){
+    //         if(prop.value = j){
+    //             console.log(prop)
+    //         }        
+    //     }
+    // }
+    
+
+    
+
+
+
+})
--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,33 @@
 console.log("Exercise 3 - Timer");
+let plus = document.getElementById("add")
+let minus = document.getElementById("sub")
+let resetbtn = document.getElementById("reset")
+let startbtn= document.getElementById("start")
+let display= document.getElementById("time")
+
+plus.addEventListener("click",function () {
+    
+    display.value++
+});
+
+minus.addEventListener("click",function () {
+    if(display.value<0){
+        display.value--
+    }
+    else{
+        alert("time has to be greater than 0")
+    }
+});
+startbtn.addEventListener("click",function (e) {
+    startbtn.textContent="stop";    
+    let timer = setInterval(() => {
+        display.value--
+        if (display.value === "0"){
+            clearInterval(timer)            
+        }
+
+    }, 1000);
+})
+resetbtn.addEventListener("click", function () {
+    display.value = 0
+})
