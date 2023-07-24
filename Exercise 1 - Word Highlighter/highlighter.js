cf01d6d22330784e6955b5fc629d4f139d4f8899
umutesimagnificat@gmail.com <umutesimagnificat@gmail.com>
2023-07-24 13:05:33 +0200

    e

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -3,6 +3,7 @@ let para = document.getElementById("myParagraph")
 let arr = para.textContent.split(" ");
 let arr2=[];
 let fiveWords=[];
+let text=[];
 // let mostOccuring=[];
 window.addEventListener("DOMContentLoaded", function(){
     var count=arr.reduce((acc, curr)=>{
@@ -18,42 +19,30 @@ window.addEventListener("DOMContentLoaded", function(){
     for(let j=1; j<6; j++){
         fiveWords.push(arr2[j][0])
     }
+
     console.log(fiveWords)
-    let word = para.childNodes
-    word.forEach((elt)=>{
-        if(elt.nodeName === "#text"){
-            let segment = elt
-            console.log (segment)
-            // highlight(elt)
-        }
+
+    // let word = para.childNodes
+    // word.forEach((elt)=>{
+    //     if(elt.nodeName === "#text"){
+    //         let segment = elt.textContent.split(" ")
+        
+    //         highlight(segment)
+    //     }
+        
     })
 
     // function highlight(text){
-    //     for
-    // }
-    
-    // console.log(word)
-    // Each entry!!
-    // for (let i in arr2){
-    //     console.log (arr2[i][0])        
-    // }
-
-    // for (let i in count){
-    //     arr2.push(count[i])
-
-    // }
-    // console.log(arr2.sort((a,b) => b-a))
-    // for (let j=1; j<6;j++){
-    //     for(let prop of count){
-    //         if(prop.value = j){
-    //             console.log(prop)
-    //         }        
+    //     for (let i =0; i<text.length;i++){
+    //         if (fiveWords.includes(text[i])){
+    //             console.log(text[i])
+                
+    //         }
     //     }
     // }
     
+    
 
     
 
 
-
-})
--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -4,30 +4,81 @@ let minus = document.getElementById("sub")
 let resetbtn = document.getElementById("reset")
 let startbtn= document.getElementById("start")
 let display= document.getElementById("time")
-
-plus.addEventListener("click",function () {
-    
-    display.value++
-});
-
-minus.addEventListener("click",function () {
-    if(display.value<0){
-        display.value--
-    }
-    else{
-        alert("time has to be greater than 0")
-    }
-});
-startbtn.addEventListener("click",function (e) {
-    startbtn.textContent="stop";    
-    let timer = setInterval(() => {
-        display.value--
-        if (display.value === "0"){
-            clearInterval(timer)            
+let container = document.getElementById("contains")
+container.addEventListener("click", function (e){
+      if(e.target === plus){
+        if (e.currentTarget === start){
+            e.preventDefault()
         }
+        display.value++;
+      }
+      if(e.target === minus){
+        
+        if(display.value>0){
+           display.value--
+            }
+            else{
+                alert("time has to be greater than 0")
+            }
+      }
+      if(e.target === start){
+          e.stopPropagation();
+          startbtn.textContent="stop";    
+          let timer = setInterval(() => {
+              if (Number(display.value) <= 0){
+                  clearInterval(timer)            
+                }
+                display.value--;
+                
+            }, 1000);
+      }
+      if(e.target === resetbtn){
+        display.value=0
+      }
 
-    }, 1000);
 })
-resetbtn.addEventListener("click", function () {
-    display.value = 0
-})
+// plus.addEventListener("click",function (event) {
+//     if (e.currentTarget.id==="start" || display.value !== "0"){
+//         event.preventDefault();
+//     }
+//     else{
+//         display.value++
+//     }
+// });
+
+// minus.addEventListener("click",function (event) {
+//     if (e.currentTarget.id==="start" || display.value !== "0"){
+//         event.preventDefault()
+//     }
+//     else{
+
+//         if(display.value>0){
+//             display.value--
+//         }
+//         else{
+//             alert("time has to be greater than 0")
+//         }
+//     }
+// });
+// startbtn.addEventListener("click",function (e) {
+//     startbtn.textContent="stop";    
+//     let timer = setInterval(() => {
+//         display.value--
+//         if (display.value === "0"){
+//             clearInterval(timer)            
+//         }
+
+//     }, 1000);
+//     if (e.currentTarget.id="start"){
+//         e.stopPropagation()
+//     }
+// })
+// resetbtn.addEventListener("click", function (event) {
+//     if (e.currentTarget.id==="start" || display.value !== "0"){
+//         event.preventDefault()
+//     }
+//     else{
+
+//         display.value = 0
+//     }
+// })
